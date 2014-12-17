0b113c13cc075db945d306c3d799daa78a7588f4
anotherangrymouse <grillopress.com@gmail.com>
2014-12-17 11:29:04 +0000

    add theme

---
+++ wordpress/wp-content/themes/wordpress-bootstrap-master/page-ae.php
@@ -0,0 +1,84 @@
+<?php
+/*
+Template Name: A&E
+*/
+?>
+
+<!doctype html>  
+
+<!--[if IEMobile 7 ]> <html <?php language_attributes(); ?>class="no-js iem7"> <![endif]-->
+<!--[if lt IE 7 ]> <html <?php language_attributes(); ?> class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html <?php language_attributes(); ?> class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html <?php language_attributes(); ?> class="no-js ie8"> <![endif]-->
+<!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
+	
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title><?php wp_title( '|', true, 'right' ); ?></title>	
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+
+		<!-- wordpress head functions -->
+		<?php wp_head(); ?>
+		<!-- end of wordpress head -->
+		<!-- IE8 fallback moved below head to work properly. Added respond as well. Tested to work. -->
+			<!-- media-queries.js (fallback) -->
+		<!--[if lt IE 9]>
+			<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>			
+		<![endif]-->
+
+		<!-- html5.js -->
+		<!--[if lt IE 9]>
+			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->	
+		
+			<!-- respond.js -->
+		<!--[if lt IE 9]>
+		          <script type='text/javascript' src="http://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.js"></script>
+		<![endif]-->	
+	</head>
+	
+	<body <?php body_class(); ?>>
+
+		
+		<div class="container">
+
+			<div id="content" class="clearfix row">
+			
+				<div id="main" class="col col-lg-12 clearfix" role="main">
+
+					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+					
+					<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
+						
+						<header>
+							
+							<div class="page-header"><h1 class="sth-heading"><?php the_title(); ?></h1></div>
+						
+						</header> <!-- end article header -->
+					
+						<section class="post_content">
+							<?php the_content(); ?>
+					
+						</section> <!-- end article section -->
+						
+						<footer>
+			
+							
+						</footer> <!-- end article footer -->
+					
+					</article> <!-- end article -->
+					
+					
+					<?php endwhile; ?>	
+					
+					<?php else : ?>
+									
+					<?php endif; ?>
+			
+				</div> <!-- end #main -->
+    
+			</div> <!-- end #content -->
+
+<?php get_footer(); ?>
